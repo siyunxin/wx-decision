904dc1a8941142553bc2b493356b620610c4773b
siyunxin <15194113617@163.com>
2021-04-21 13:50:09 +0800

    first uiapp wx

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,8 +1,7 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
 		<view>
-			<text class="title">{{title}}</text>
+			<text class="title">哈哈哈哈哈{{title}}</text>
 		</view>
 	</view>
 </template>
